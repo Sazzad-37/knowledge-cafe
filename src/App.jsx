b6619b26af32dd3b25b0dc16b203acf9c1bcaaf7
MdSazzadHossain37 <sazzadhossain2026@gmail.com>
2024-04-13 04:33:56 +0600

    load data from blogs.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
 import Header from './components/header/Header'
 
 function App() {
   return (
     <>
     <Header></Header>
+    <Blogs></Blogs>
       
     </>
   )
